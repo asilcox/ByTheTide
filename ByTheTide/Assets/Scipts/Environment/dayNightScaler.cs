8c51e71b5674a3496c83d1d03f727a7679a24883
Churchy0429 <Dosen260@gmail.com>
2022-03-07 12:55:15 -0500

    day/night sys

--- ByTheTide/Assets/Scipts/Environment/dayNightScaler.cs
+++ ByTheTide/Assets/Scipts/Environment/dayNightScaler.cs
@@ -2,66 +2,13 @@ using UnityEngine;
 
 public class dayNightScaler : MonoBehaviour
 {
-    [SerializeField] float nightTimeMax;
-    [SerializeField] float currentNightTime;
     [SerializeField] float rotSpeed;
     [SerializeField] Vector3 rotDirection;
-    [SerializeField] timeStates currentTimeState;
-    [SerializeField] timeStates previousState;
-    [SerializeField] Vector3 forwardRotSwitch;
-    [SerializeField] Vector3 reverseRotSwitch;
     // Update is called once per frame
     void Update()
     {
-        switch (currentTimeState)
-        {
-            case timeStates.forward:
-                {
-                    transform.Rotate(rotDirection * rotSpeed * Time.deltaTime);
-                    if (transform.eulerAngles.x >= forwardRotSwitch.x)
-                    {
-                        previousState = timeStates.forward;
-                        currentTimeState = timeStates.nightTime;
-                    }
-                    break;
-                }
-            case timeStates.reverse:
-                {
-                    transform.Rotate(-rotDirection * rotSpeed * Time.deltaTime);
-                    if (transform.eulerAngles.x <= reverseRotSwitch.x)
-                    {
-                        previousState = timeStates.reverse;
-                        currentTimeState = timeStates.nightTime;
-                    }
-                    break;
-                }
-            case timeStates.nightTime:
-                {
-                    currentNightTime = currentNightTime - 1 * Time.deltaTime;
-                    if(currentNightTime <= 0)
-                    {
-                       if(previousState == timeStates.forward)
-                        {
-                            currentNightTime = nightTimeMax;
-                            currentTimeState = timeStates.reverse;
-                        }
-                       else
-                        {
-                            currentNightTime = nightTimeMax;
-                            currentTimeState = timeStates.forward;
-                        }
-                    }
-                    break;
-                }
-
-        }
+        transform.Rotate(rotDirection * rotSpeed * Time.deltaTime);
     }
 
-    public enum timeStates
-    {
-        forward,
-        reverse,
-        nightTime,
-    }
 }
 
