cab14b175049f127d73f91e6ae5f163fbca0b266
jlwalker653 <jlwalker2@student.fullsail.edu>
2022-03-09 21:48:10 -0600

    Update
    Finished Save System and setup HUB system.

--- ByTheTide/Assets/Scripts/Managers/SaveSystemScript.cs
+++ ByTheTide/Assets/Scripts/Managers/SaveSystemScript.cs
@@ -7,7 +7,11 @@ using System.Text.RegularExpressions;
 public class SaveSystemScript : MonoBehaviour
 {
     private Vector3 SaveCurPos;
+    private Vector3 SaveCurRot;
     private Vector3 HubIslandPos;
+    private Vector3 HubIslandRot;
+
+    private CharacterController CC;
 
     private int Lives;
     private int isDead;
@@ -15,33 +19,46 @@ public class SaveSystemScript : MonoBehaviour
     private int HighLevel;
     private int CurLevel;
 
-    [SerializeField] private GameObject[] Checkpoints;
-    private int CurCheckpoint;
     private string curCheckpointName;
-
 
     private void Start()
     {
         Scene scene = SceneManager.GetActiveScene();
         CurLevel = scene.buildIndex;
+        CC = GetComponent<CharacterController>();
 
         Lives = PlayerPrefs.GetInt("Lives");
 
-        if (scene.name != "HubLevel")
+        if (scene.name != "hub_level")
         {
             if (isDead == 0)
             {
+                CC.enabled = false;
                 SaveCurPos.x = PlayerPrefs.GetFloat("CurPosX", SaveCurPos.x);
                 SaveCurPos.y = PlayerPrefs.GetFloat("CurPosY", SaveCurPos.y);
                 SaveCurPos.z = PlayerPrefs.GetFloat("CurPosZ", SaveCurPos.z);
-            }
 
-            isDead = 0;
+                SaveCurRot.x = PlayerPrefs.GetFloat("CurRotX", SaveCurRot.x);
+                SaveCurRot.y = PlayerPrefs.GetFloat("CurRotY", SaveCurRot.y);
+                SaveCurRot.z = PlayerPrefs.GetFloat("CurRotZ", SaveCurRot.z);
 
-            if (SaveCurPos.x != 0 && SaveCurPos.y != 0 && SaveCurPos.z != 0)
-            {
+                curCheckpointName = PlayerPrefs.GetString("curCheckpointName", curCheckpointName);
+
+                GameObject go_CheckPoint = GameObject.Find(curCheckpointName);
+                if (go_CheckPoint != null)
+                {
+                    curCheckPointGO = go_CheckPoint;
+                    curCheckPointGO.SetActive(false);
+                }
+
+                Debug.Log(SaveCurRot);
+
                 gameObject.transform.position = SaveCurPos;
+                gameObject.transform.Rotate(SaveCurRot);
+                CC.enabled = true;
             }
+            SaveCheckPoint();
+            isDead = 0;
 
             HighLevel = PlayerPrefs.GetInt("HighLevel");
 
@@ -54,25 +71,31 @@ public class SaveSystemScript : MonoBehaviour
             HubIslandPos.y = PlayerPrefs.GetFloat("HubIslandPosY", HubIslandPos.y);
             HubIslandPos.z = PlayerPrefs.GetFloat("HubIslandPosZ", HubIslandPos.z);
 
+            HubIslandRot.x = PlayerPrefs.GetFloat("HubIslandRotX", HubIslandRot.x);
+            HubIslandRot.y = PlayerPrefs.GetFloat("HubIslandRotY", HubIslandRot.y);
+            HubIslandRot.z = PlayerPrefs.GetFloat("HubIslandRotZ", HubIslandRot.z);
+
             PlayerPrefs.SetInt("Lives", 3);
             gameObject.transform.position = HubIslandPos;
+            gameObject.transform.Rotate(HubIslandRot);
 
-            CurCheckpoint = 0;
-            PlayerPrefs.SetInt("curCheckPoint", CurCheckpoint);
+            curCheckpointName = "";
+            PlayerPrefs.SetString("curCheckpointName", curCheckpointName);
         }
     }
+
+    private GameObject curCheckPointGO;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "CheckPoint")
         {
-            SaveCurPos = gameObject.transform.position;
             curCheckpointName = other.gameObject.name;
-            curCheckpointName = Regex.Replace(curCheckpointName, "[^0-9]", "");
-            CurCheckpoint = int.Parse(curCheckpointName);
-            PlayerPrefs.SetInt("curCheckPoint", CurCheckpoint);
+            PlayerPrefs.SetString("curCheckpointName", curCheckpointName);
+            GameObject go_CheckPoint = GameObject.Find(curCheckpointName);
+            curCheckPointGO = go_CheckPoint;
+
             SaveCheckPoint();
-            other.gameObject.SetActive(false);
         }
 
         if (other.gameObject.tag == "Hazard")
@@ -81,23 +104,55 @@ public class SaveSystemScript : MonoBehaviour
 
     private void SaveCheckPoint()
     {
+        SaveCurPos = transform.position;
+        SaveCurRot.x = transform.eulerAngles.x;
+        SaveCurRot.y = transform.eulerAngles.y;
+        SaveCurRot.z = transform.eulerAngles.z;
+
+        Debug.Log(SaveCurRot);
+
         PlayerPrefs.SetFloat("CurPosX", SaveCurPos.x);
         PlayerPrefs.SetFloat("CurPosY", SaveCurPos.y);
         PlayerPrefs.SetFloat("CurPosZ", SaveCurPos.z);
+
+        PlayerPrefs.SetFloat("CurRotX", SaveCurRot.x);
+        PlayerPrefs.SetFloat("CurRotY", SaveCurRot.y);
+        PlayerPrefs.SetFloat("CurRotZ", SaveCurRot.z);
+
+        curCheckPointGO.SetActive(false);
+    }
+
+    public void CompletedLevel()
+    {
+        if (CurLevel == HighLevel)
+            HighLevel++;
+        else
+            return;
+
+        PlayerPrefs.SetInt("HighLevel", HighLevel);
     }
 
     public void SaveHubLocation()
     {
+        HubIslandPos = transform.position;
+        HubIslandRot.x = transform.eulerAngles.x;
+        HubIslandRot.y = transform.eulerAngles.y;
+        HubIslandRot.z = transform.eulerAngles.z;
+
         PlayerPrefs.SetFloat("HubIslandPosX", HubIslandPos.x);
         PlayerPrefs.SetFloat("HubIslandPosY", HubIslandPos.y);
         PlayerPrefs.SetFloat("HubIslandPosZ", HubIslandPos.z);
+
+        PlayerPrefs.SetFloat("HubIslandRotX", HubIslandRot.x);
+        PlayerPrefs.SetFloat("HubIslandRotY", HubIslandRot.y);
+        PlayerPrefs.SetFloat("HubIslandRotZ", HubIslandRot.z);
     }
 
     private void CheckLives()
     {
         Scene scene = SceneManager.GetActiveScene();
 
-        if (scene.name != "HubLevel")
+        if (scene.name != "hub_level")
         {
             Lives--;
             PlayerPrefs.SetInt("Lives", Lives);
@@ -109,6 +164,11 @@ public class SaveSystemScript : MonoBehaviour
             PlayerPrefs.SetFloat("CurPosX", 0);
             PlayerPrefs.SetFloat("CurPosY", 0);
             PlayerPrefs.SetFloat("CurPosZ", 0);
+
+            PlayerPrefs.SetFloat("CurRotX", 0);
+            PlayerPrefs.SetFloat("CurRotY", 0);
+            PlayerPrefs.SetFloat("CurRotZ", 0);
+
             PlayerPrefs.SetInt("Lives", 3);
         }
 
--- ByTheTide/Assets/Scripts/SceneTransitionScript.cs
+++ ByTheTide/Assets/Scripts/SceneTransitionScript.cs
@@ -6,10 +6,19 @@ using UnityEngine.SceneManagement;
 public class SceneTransitionScript : MonoBehaviour
 {
     public string LevelToLoad;
+    [SerializeField] SaveSystemScript SSS;
+
+    private void Start()
+    {
+        SSS = SSS.GetComponent<SaveSystemScript>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
+        {
+            SSS.SaveHubLocation();
             SceneManager.LoadScene(LevelToLoad);
+        }
     }
 }
