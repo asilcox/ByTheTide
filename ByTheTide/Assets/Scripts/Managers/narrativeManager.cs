25cff835597b069d0e2806ff4836ad45ba295f40
JRGermain93 <JRGermain@student.fullsail.edu>
2022-03-19 11:44:46 -0400

    Tutorial Level Script
    The script shows I'm so freakin happy!!!!

--- ByTheTide/Assets/Scripts/Managers/narrativeManager.cs
+++ ByTheTide/Assets/Scripts/Managers/narrativeManager.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 using TMPro;
 public class narrativeManager : MonoBehaviour
 {
-    [SerializeField] playerGameStates currentState; //The current state of the enum
+    //I am not sure if I am supposed to put anything in this section. Google and my brain are failing me. JG
+    [SerializeField] GameObject narrativePannel;
+    [SerializeField] public playerGameStates currentState;  //The current state of the enum
     [SerializeField] string[] dialogues; //Dialogue
     [SerializeField] int dialogueIndex; //Identifies which dialogue in the array we want to access
     [SerializeField] TextMeshProUGUI dialogueTextObj; //The text object to be assigned in engine
@@ -21,18 +23,21 @@ public class narrativeManager : MonoBehaviour
         {
             case playerGameStates.inPlay:
                 {
-                    //Should deactivate text here
+                    narrativePannel.gameObject.SetActive(false);
+                    dialogueTextObj.gameObject.SetActive(false);
                     break;
                 }
             case playerGameStates.textPop:
                 {
-                    //display text here
+                    narrativePannel.SetActive(true);
+                    dialogueTextObj.gameObject.SetActive(true);
+                    dialogueTextObj.text = dialogues[dialogueIndex];
                     break;
                 }
         }
     }
 
-    void updateString()
+   public void updateString()
     {
         if(dialogueIndex == dialogues.Length - 1)
         {
@@ -44,13 +49,6 @@ public class narrativeManager : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject.tag == "narrative")
-        {
-            //we just stepped into an area with narrative! Lets activate the text box and ensure we have the proper string index
-        }
-    }
 
     public enum playerGameStates
     {
