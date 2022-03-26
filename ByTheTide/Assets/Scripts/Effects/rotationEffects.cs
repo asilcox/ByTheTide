f50a7db12838d24e6bba62d8ba0f0e7ad2d3eafc
Churchy0429 <Dosen260@gmail.com>
2022-03-26 18:50:36 -0400

    Rotation fix, circle camera fix
    Rotation no longer childs camera, Camera no longer has circle effect in hub

--- ByTheTide/Assets/Scripts/Effects/rotationEffects.cs
+++ ByTheTide/Assets/Scripts/Effects/rotationEffects.cs
@@ -7,24 +7,24 @@ public class rotationEffects : MonoBehaviour
     [SerializeField] Vector3 rotationDir;
     [SerializeField] float rotationSpeed;
 
-    private void OnTriggerStay(Collider other)
-    {
-        if(other.gameObject.tag == "Player")
-        {
-            other.gameObject.transform.SetParent(this.gameObject.transform);
-            other.gameObject.transform.localScale.Set(1,1,1);
-        }
-    }
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    if(other.gameObject.tag == "Player")
+    //    {
+    //        other.gameObject.transform.SetParent(this.gameObject.transform);
+    //        other.gameObject.transform.localScale.Set(1,1,1);
+    //    }
+    //}
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            other.gameObject.transform.localScale.Set(1, 1, 1);
-            other.gameObject.transform.SetParent(null);
-        }
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if (other.gameObject.tag == "Player")
+    //    {
+    //        other.gameObject.transform.localScale.Set(1, 1, 1);
+    //        other.gameObject.transform.SetParent(null);
+    //    }
 
-    }
+    //}
 
     private void FixedUpdate()
     {
